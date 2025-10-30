acbc3bf4f4c760e2790863d9925d4273daab4fa5
Daxomar <152349184+Daxomar@users.noreply.github.com>
2025-10-30 06:21:40 +0000

    screen fitting

--- src/Components/projectsSection.jsx
+++ src/Components/projectsSection.jsx
@@ -15,7 +15,7 @@ const ProjectSection = () => {
 
 
 
-            {/* <div className="flex gap-6 mt-10">
+            <div className="flex gap-6 mt-10">
                 <div className="flex flex-col justify-end w-[602px] h-[366px] bg-RonPrimary rounded-RButton">
                     <div className=" text-RButtonText font-RMontserrat text-start px-4 py-10 text-RPrimary ">HENESSY PRO - 3D Architecture</div>
                    
@@ -26,63 +26,23 @@ const ProjectSection = () => {
                     <div className=" text-RButtonText font-RMontserrat text-start px-4 py-10 text-RPrimary ">HENESSY PRO - 3D Architecture</div>
                     
                 </div>
-            </div> */}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            <div className="flex gap-6 mt-10 overflow-x-auto scrollbar-hide px-4">
-                <div className="flex flex-col justify-end w-[602px] h-[366px] bg-RonPrimary rounded-RButton ">
-                    <div className="text-RButtonText font-RMontserrat text-start px-4 py-10 text-RPrimary">
-                        HENESSY PRO - 3D Architecture
-                    </div>
-                </div>
-
-                <div className="flex flex-col justify-end w-[602px] h-[366px] bg-RonPrimary rounded-RButton ">
-                    <div className="text-RButtonText font-RMontserrat text-start px-4 py-10 text-RPrimary">
-                        MODERN VILLA - 3D Design
-                    </div>
-                </div>
-
-                <div className="flex flex-col justify-end w-[602px] h-[366px] bg-RonPrimary rounded-RButton">
-                    <div className="text-RButtonText font-RMontserrat text-start px-4 py-10 text-RPrimary">
-                        LUXURY INTERIOR - 3D Visualization
-                    </div>
-                </div>
             </div>
 
 
 
-            <div className="flex gap-6 mt-10 overflow-x-auto scrollbar-hide px-4">
-                <div className="flex flex-col justify-end w-[602px] h-[366px] bg-RonPrimary rounded-RButton ">
-                    <div className="text-RButtonText font-RMontserrat text-start px-4 py-10 text-RPrimary">
-                        HENESSY PRO - 3D Architecture
-                    </div>
-                </div>
 
-                <div className="flex flex-col justify-end w-[602px] h-[366px] bg-RonPrimary rounded-RButton ">
-                    <div className="text-RButtonText font-RMontserrat text-start px-4 py-10 text-RPrimary">
-                        MODERN VILLA - 3D Design
-                    </div>
-                </div>
 
-                <div className="flex flex-col justify-end w-[602px] h-[366px] bg-RonPrimary rounded-RButton ">
-                    <div className="text-RButtonText font-RMontserrat text-start px-4 py-10 text-RPrimary">
-                        LUXURY INTERIOR - 3D Visualization
-                    </div>
-                </div>
-            </div>
+
+
+
+
+
+
+
+
+
+
+           
 
 
         </div>
