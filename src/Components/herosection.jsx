d4342bbcc7e1dac8162e83a9f42d6a0b474e92fd
Daxomar <152349184+Daxomar@users.noreply.github.com>
2025-11-02 03:22:33 +0000

    Doing responsive IntroSection

--- src/Components/herosection.jsx
+++ src/Components/herosection.jsx
@@ -14,9 +14,9 @@ const Herosection = () => {
                       w-screen 
                       bg-[url('/BGMobile.png')]
     sm:bg-[url('/Background.png')]
-                      h-[calc(100vh)]    
+                      h-[calc(100dvh)]    
                      
-                    
+                      mt-[-80px]
                       sm:mt-[-20px]                                           
                       "
         >
@@ -26,7 +26,7 @@ const Herosection = () => {
 
             <div className=" justify-between items-end flex sm:gap-2  flex-col  text-start h-full w-full  sm:justify-center sm:items-center  sm:mt-32  sm:w-screen sm:h-screen    ">
                 <div className=" px-[20px] sm:p-0 order-2 sm:order-1 text-white sm:text-black text-RHeader1 font-RMontserrat w-full  sm:w-full flex  sm:justify-center sm:items-center ">
-                    I am a<br/>Digital Designer
+                    I am a<br/>Digital Designerrr
                 </div>
 
                 <div className="  order-1 sm:order-2 sm:w-full  h-full sm:h-auto sm:text-black flex  text-3xl gap-2 flex-col justify-center items-start px-12 ">
