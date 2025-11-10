b2eb3dee955944c3484ce6d471d87485bbd8fa75
Daxomar <152349184+Daxomar@users.noreply.github.com>
2025-11-10 19:58:35 +0000

    Added making screen more responsive

--- src/Components/introSection.jsx
+++ src/Components/introSection.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const IntroSection = () => {
   return (
-   <div className="border-b-4 space-y-8 border-RPrimary flex flex-col justify-center w-full text-RPrimary mt-16 sm:h-screen max-w-[1980px] max-h-[1080px] ">
+   <div className="border-b-4  justify-center   max-h-[1080px]  border-t-4 border-RonPrimary  pb-16  space-y-8 flex flex-col text-RPrimary  w-full   mt-16 max-w-[1980px] ">
                  <div className="flex  ">
                     <div className="  flex gap-2 items-center w-full h-[64px] sm:mt-16 ">
                         <div className=""><i class="fa-solid fa-star text-2xl"></i></div>
