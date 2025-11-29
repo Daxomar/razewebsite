13f11fb9e94ceef9debf9b55b5fda7a21c3acb81
Daxomar <152349184+Daxomar@users.noreply.github.com>
2025-11-29 20:13:46 +0000

    Added Admin section finally

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -18,11 +18,11 @@ const Home = () => {
         <div className="flex flex-col items-center flex-1 bg-RonPrimary py-[80px] sm:py-[120px] px-[20px] sm:px-[40px] md:px-[40px] lg:px-[80px] 2xl:px-[100px] ">
             <Herosection />
             <IntroSection />
-            <ServiceSection />
-            <PricingSection/>
+            {/* <ServiceSection /> */}
+            {/* <PricingSection/> */}
             {/* <HighLightSection /> */}
-            <TestimonialSection/>
-            <ProjectSection />
+            {/* <TestimonialSection/> */}
+            {/* <ProjectSection /> */}
             
             
    {/* <video
